10eb8272b436f7c52a3ac77b68d1751dec5907bd
adminZapAer <zapata.camiones.redes@gmail.com>
2020-01-26 02:19:25 -0600

    mejora en el buscador y pendiente lo del registro de compras por medio de transferencia, registra los datos en la tabla pagosTransferencia, falta registrar acorde al numero de articulos

--- frontend/vistas/modulos/pagoTransferencia.php
+++ frontend/vistas/modulos/pagoTransferencia.php
@@ -59,106 +59,100 @@ BREADCRUMB TRANSFERENCIA
                             
                             <?php
                                     
-                                    $item = $_SESSION["idUsuario"];
-                                    
-                                    $user = ControladorUsuarios::ctrMostrarUsuario("idUsuario", $item);
-                                    //var_dump($user);
-                                    
-                                    //var_dump($direccion[0]);
-                                    
-                                    echo '
-                                    
-                                    <input type="hidden" value="'.$_SESSION["idUsuario"].'" id="idUsuario" name="idUsuario">
-
-                                    <h3>Los campos marcados con * son obligatorios.</h3>
-
-                                    <label class="control-label text-muted text-uppercase" for="usuarioComprobante">Nombre del Usuario *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="glyphicon glyphicon-user"></i>
-                                        </span>
-                                        <input type="text" class="form-control" id="usuarioComprobante" name="usuarioComprobante" placeholder="Nombre completo" value = "'.$user["nombre"].'" readonly>
-                                    </div>
-
-                                    <br>
-
-                                    <label class="control-label text-muted text-uppercase" for="fechaPago">Fecha de Pago *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="glyphicon glyphicon-user"></i>
-                                        </span>
-                                        <input type="text" class="form-control" id="fechaPago" name="fechaPago" placeholder="0000-00-00 00:00:00" required>
-                                    </div>
-
-                                    <br>
-
-                                    <label class="control-label text-muted text-uppercase" for="banco">Banco *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="glyphicon glyphicon-home"></i>
-                                        </span>
-                                        <div class="form-group">
-                                            <select class="form-control" id="banco" name="banco" placeholder="Banco" required>
-                                                <option>Elije una opción</option>
-                                                <option>BBVA Bancomer</option>
-                                                <option>Banamex</option>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    <br>
-
-                                    <label class="control-label text-muted text-uppercase" for="montoExacto">Monto Exacto *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="glyphicon glyphicon-home" aria-hidden="true"></i>
-
-                                        </span>
-                                        <input type="text" class="form-control" id="montoExacto" name="montoExacto" placeholder="Monto Exacto" required>
-                                    </div>
-
-                                    <br>
-
-                                    <label class="control-label text-muted text-uppercase" for="referencia">Referencia *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="glyphicon glyphicon-home" aria-hidden="true"></i>
-
-                                        </span>
-                                        <input type="text" class="form-control" id="referencia" name="referencia" placeholder="Referencia">
-                                    </div>
-
-                                    <br>
-
-                                    <label class="control-label text-muted text-uppercase" for="datosRecibo">Subir Foto de Recibo *</label>
-
-                                    <div class="input-group">
-                                        <span class="input-group-addon">
-                                            <i class="fa fa-university" aria-hidden="true"></i>
-
-                                        </span>
-                                        <input type="file" class="form-control" id="datosRecibo" name="datosRecibo">
-                                        
-                                    </div>
-                                    <img class="previsualizarRecibo">
-                                    
-                                    <br>
-                                    <br>
-                                    <br>
-                                    
-                                    <button class="btn btn-success btn-md pull-left updat">Cargar Datos de Recibo</button>
-                                    
-                                    <br>
-                                    <br>
-                                    ';
-                                    
+                            $item = $_SESSION["idUsuario"];
+                            
+                            $user = ControladorUsuarios::ctrMostrarUsuario("idUsuario", $item);
+                            //var_dump($user);
+                            
+                            //var_dump($direccion[0]);
+                            
+                            echo '
+                            
+                            <input type="hidden" value="'.$_SESSION["idUsuario"].'" id="idUsuario" name="idUsuario">
+                            
+                            <h3>Los campos marcados con * son obligatorios.</h3>
+                            
+                            <label class="control-label text-muted text-uppercase" for="usuarioComprobante">Nombre del Usuario *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                </span>
+                                <input type="text" class="form-control" id="usuarioComprobante" name="usuarioComprobante" placeholder="Nombre completo" value = "'.$user["nombre"].'" readonly>
+                            </div>
+                            
+                            <br>
+                            
+                            <label class="control-label text-muted text-uppercase" for="fechaPago">Fecha de Pago *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                </span>
+                                <input type="text" class="form-control" id="fechaPago" name="fechaPago" placeholder="0000-00-00 00:00:00" required>
+                            </div>
+                            
+                            <br>
+                            
+                            <label class="control-label text-muted text-uppercase" for="banco">Banco *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-home"></i>
+                                </span>
+                                <div class="form-group">
+                                    <select class="form-control" id="banco" name="banco" placeholder="Banco" required>
+                                        <option>Elije una opción</option>
+                                        <option>BBVA Bancomer</option>
+                                        <option>Banamex</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <br>
+                            
+                            <label class="control-label text-muted text-uppercase" for="montoExacto">Monto Exacto *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-home" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control" id="montoExacto" name="montoExacto" placeholder="Monto Exacto" required>
+                            </div>
+                            
+                            <br>
+                            
+                            <label class="control-label text-muted text-uppercase" for="referencia">Referencia *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="glyphicon glyphicon-home" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" class="form-control" id="referencia" name="referencia" placeholder="Referencia">
+                            </div>
+                            
+                            <br>
+                            
+                            <label class="control-label text-muted text-uppercase" for="datosRecibo">Subir Foto de Recibo *</label>
+                            
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <i class="fa fa-university" aria-hidden="true"></i>
+                                </span>
+                                <input type="file" class="form-control" id="datosRecibo" name="datosRecibo">
                                 
-                                
+                            </div>
+                            <img class="previsualizarRecibo">
+                            
+                            <br>
+                            <br>
+                            <br>
+                            
+                            <button class="btn btn-success btn-md pull-left updat">Cargar Datos de Recibo</button>
+                            
+                            <br>
+                            <br>
+                            ';
                             
                             ?>
                             
