871da5841c9a6fcc74747f304092fdd1a2a63713
AdminZapAer <zapata.camiones.redes@gmail.com>
2020-04-06 16:53:10 -0500

    cambios en ofertas y en encabezados

--- frontend/vistas/modulos/ofertas.php
+++ frontend/vistas/modulos/ofertas.php
@@ -78,7 +78,7 @@ $ruta = $rutas[0];
                                 
                                 <figure>';
                                     
-                                    if($value["imgOferta"] != "" || $value["imgOferta"] != null){
+                                    if($value["imgOferta"] != ""){
                                         echo '<img class="img-responsive" src="'.$servidor.$value["imgOferta"].'"width="100%">';
                                     }
                                     else{
