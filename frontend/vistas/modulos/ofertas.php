582e3dc543c7bb2871c16c0773ff7ba91344da55
adminZapAer <zapata.camiones.redes@gmail.com>
2020-04-06 18:09:29 -0500

    ajuste en ofertas

--- frontend/vistas/modulos/ofertas.php
+++ frontend/vistas/modulos/ofertas.php
@@ -268,7 +268,7 @@ $ruta = $rutas[0];
                                 
                                 <figure>';
                                     
-                                    if($value["imgOferta"] != "" || $value["imgOferta"] != Null || $value["imgOferta"] != " "){
+                                    if($value["imgOferta"] != "" || $value["imgOferta"] != null || $value["imgOferta"] != " "){
                                         echo'<img class="img-responsive" src="'.$servidor.$value["imgOferta"].'" width="100%">';
                                     }
                                     else{
