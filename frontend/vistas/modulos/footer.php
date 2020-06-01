610c91e73c1f21b995f8be0f3cd381d03de0c4f5
adminZapAer <zapata.camiones.redes@gmail.com>
2020-05-31 22:39:35 -0500

    Se añadio seccion Acerca de

--- frontend/vistas/modulos/footer.php
+++ frontend/vistas/modulos/footer.php
@@ -1,5 +1,48 @@
-<div class="container-fluid dudasYSugerencias">
+<div class="container">
     
+    <h2 style="text-align:center">Acerca de</h2>
+    <div class="row">
+      <div class="tarjetas">
+        <div class="card">
+          <img src="<?php echo $url;?>vistas/img/64-anios.jpg" alt="Jane" style="width:100%" >
+          <div class="container">
+            <h2>64 Años</h2>
+            <p class="title">en el mercado mexicano.</p>
+            <!--<p>Some text that describes me lorem ipsum ipsum</p>
+            <p>jane@example.com</p>-->
+          </div>
+        </div>
+      </div>
+
+      <div class="tarjetas">
+        <div class="card">
+          <img src="<?php echo $url;?>vistas/img/clientes.jpg" alt="Mike" style="width:100%">
+          <div class="container">
+            <h2>100,000</h2>
+            <p class="title">Clientes satisfechos</p>
+            <!--<p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>mike@example.com</p>-->
+          </div>
+        </div>
+      </div>
+
+      <div class="tarjetas">
+        <div class="card">
+          <img src="<?php echo $url;?>vistas/img/empresa.jpg" alt="John" style="width:100%">
+          <div class="container">
+            <h2>Empresa Socialmente</h2>
+            <p class="title">Responsable</p>
+            <!--<p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>john@example.com</p>-->
+          </div>
+        </div>
+      </div>
+    </div>
+
+</div>
+
+<div class="container-fluid dudasYSugerencias">
+
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-8 metodosPago">
             
         <ul class="horizontal">
--- frontend/vistas/template.php
+++ frontend/vistas/template.php
@@ -112,14 +112,16 @@
         </script>
 
         <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WL5MQDR');</script>
-<!-- End Google Tag Manager -->
-        
-        
+        <script>
+            (function(w,d,s,l,i)
+                {w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);}
+            )(window,document,'script','dataLayer','GTM-WL5MQDR');
+        </script>
+        <!-- End Google Tag Manager -->
 
         <!--++++++++++++++++++++++++++++++++++++++++++++++++++-->
         
@@ -357,6 +359,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 include "modulos/aplicacion.php";
                 include "modulos/marca.php";
                 include "modulos/tipo-de-sistema.php";
+                include "modulos/infoZapata.php";
             }
             else{
                 include "modulos/error404.php";
@@ -370,6 +373,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             include "modulos/aplicacion.php";
             include "modulos/marca.php";
             include "modulos/tipo-de-sistema.php";
+            include "modulos/infoZapata.php";
         }
         include "modulos/footer.php";
         ?>
