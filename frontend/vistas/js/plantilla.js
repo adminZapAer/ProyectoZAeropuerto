ae6874c80466792c3ab89b393ae48b11fd1198a5
AdminZapAer <zapata.camiones.redes@gmail.com>
2020-09-07 13:24:51 -0500

    solucionado error en marcado de paginacion

--- frontend/vistas/js/plantilla.js
+++ frontend/vistas/js/plantilla.js
@@ -120,11 +120,7 @@ var url = window.location.href; //Es para saber en que pagina me encuentro
 
 var indice = url.split("/");
 
-console.log("Indice: ", indice);
-
-var pagActual = indice[6];
-
-console.log("pag Actual ",pagActual);
+var pagActual = indice[5];
 
 if(isNaN(pagActual)){
     $("#item1").addClass("active");
