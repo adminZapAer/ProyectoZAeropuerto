f9b1abfb550d78f8f297c8c9cb3c872903c4b82f
adminZapAer <zapata.camiones.redes@gmail.com>
2020-09-08 01:57:25 -0500

    adaptacion en paginacion 1

--- frontend/vistas/js/plantilla.js
+++ frontend/vistas/js/plantilla.js
@@ -120,7 +120,12 @@ var url = window.location.href; //Es para saber en que pagina me encuentro
 
 var indice = url.split("/");
 
-var pagActual = indice[5];
+if(indice[2] =="localhost"){
+    var pagActual = indice[6];
+}
+else{
+    var pagActual = indice[5];
+}
 
 if(isNaN(pagActual)){
     $("#item1").addClass("active");
