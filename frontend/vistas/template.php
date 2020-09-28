e4c333e4e23f8236dd9c191c92fadbfbb61751e2
AdminZapAer <zapata.camiones.redes@gmail.com>
2020-09-28 15:07:24 -0500

    se agrego la etiqueta de chat de facebook

--- frontend/vistas/template.php
+++ frontend/vistas/template.php
@@ -221,6 +221,32 @@
     </head>
     <body>
         
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+        <!-- Your Chat Plugin code -->
+        <div class="fb-customerchat"
+          attribution=install_email
+          page_id="104927631189643"
+            logged_in_greeting="Bienvenido/a a nuestra Refaccionaria Zapata. &#xbf;en que podemos ayudarte?"
+            logged_out_greeting="Bienvenido/a a nuestra Refaccionaria Zapata. &#xbf;en que podemos ayudarte?">
+        </div>
+        
         <?php
         /*==========================================
                         ENCABEZADO
@@ -424,4 +450,6 @@
         </script>
         
     </body>
+    
+    
 </html>
